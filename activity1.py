d9e40308a8b14c2573576c3a86845ec0db759aed
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-01-01 17:45:48 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,8 @@
+#this is comment
+print("hello world")
+#we can display our name at console
+print("I am Shreyansh,learning Python")
+#hobby
+print("I love singing")
+#adding string and numbers together
+print("the sum of 3 and 5 is",3+5)
---
+++ activity4.py
@@ -0,0 +1,9 @@
+print("welcome to the world of programming")
+
+print(6)
+
+print("/n hello world")
+
+import keyword
+print(keyword.kwlist)
+
