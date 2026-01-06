197b98fda105deddea68144e4a7c9a3d22284629
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-01-06 17:40:30 +0530

    Add files via upload

---
+++ 6thjan.py
@@ -0,0 +1,20 @@
+name="Penguin"
+age=15
+is_student = True
+weight=38.5
+
+print("name:",name)
+print("data type of name is",type(name))
+print("age is:",age)
+print("data type of age is",type(age))
+print("is student:",is_student)
+print("data type of is_student is",type(is_student))
+print("weight is:",weight)
+print("data type of weight is",type(weight))
+
+print("/n after type casting....")
+age=str(age)
+print(age)
+print("data type of age is",type(age))
+weight=int(weight)
+print("data type of weight is",type(weight))
