a15d040375773cf85e7b1f6525960d9af29de7f0
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-01-20 17:54:56 +0530

    Add files via upload

---
+++ 20thjan2.py
@@ -0,0 +1,24 @@
+import turtle
+
+t=turtle.Turtle()
+s=turtle.Screen()
+colors= ["red","purple","blue","green","orange","yellow"]
+s.bgcolor("black")
+t.speed("fastest")
+t.hideturtle("fastest")
+while True:
+    for x in range(200):
+        t.pencolor(colors[x%len(colors)])
+        t.width(x/100 + 1)
+        t.forward(x)
+        t.left(67)
+        t.right(269)
+for x in range(200,0,-1):
+    t.pencolor("black")
+    t.width(x/100 +7)
+    t.forward(x)
+    t.right(59)
+        
+
+
+
