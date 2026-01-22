de99147a8e49a98b6ae33a665e3bf96909a85e7f
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-01-22 17:40:24 +0530

    Add files via upload

---
+++ 22jan.py
@@ -0,0 +1,29 @@
+list1=['apple','banana','cherry','pineapple','kiwi']
+print("length of list",len(1ist1))
+print("First element:",list1[0])
+print("last element:",list1[-1])
+
+list1.append("papaya")
+print("updated list:",list1)
+
+list1.remove("Guava")
+print("updated list:",list1)
+
+list1.sort()
+print("sorted list",list1)
+
+list.pop(1)
+print("Updated list:",list1)
+
+list1.reverse()
+print("reversed list:",list1)
+
+print("multiplication on list:",list1*2)
+
+list1=list1[:4]
+print("sliced list:"list1)
+
+list1.clear()
+print("updated list:",list1)
+
+
