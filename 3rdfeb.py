07c1606ea3b2b6a1a4c7bd1b4c8d93e1f7ed2105
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-02-03 17:23:39 +0530

    Add files via upload

---
+++ 3rdfeb.py
@@ -0,0 +1,13 @@
+class Employee:
+
+    def __init__(self):
+        print("Employee created")
+
+    def __del__(self):
+        print("Destructor called, Employee deleted")
+
+obj = Employee()
+
+del obj
+
+
