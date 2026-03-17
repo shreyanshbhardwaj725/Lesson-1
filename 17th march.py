728cdc97e1d276358a840029711a997797aa748f
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-03-17 17:31:51 +0530

    Add files via upload

---
+++ 17th march.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration = 0
+    print("The number entered by the user is: ",n)
+    iteration += 6
+    print("The total iteration done by code is ", iteration)
+
+printnumber(10)
+printnumber(10)
