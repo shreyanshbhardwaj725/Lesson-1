d32e0998717e649d2ea91a23c7d5eea0c79bb4f9
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-03-26 17:28:42 +0530

    Add files via upload

---
+++ 26march2.py
@@ -0,0 +1,10 @@
+numlargest=int(input('enter the largest number: '))
+numsmallest=int(input("enter the smallest number: "))
+
+
+while(number smallest):
+    numberStore = numberSmallest
+    numberSmallest = numberLargest % numberSmallest
+    numberLargest = numberStore
+
+print("HCF is : ",numberLargest)
