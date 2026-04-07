7351639c204595525e2d53343dcb2eadce977fc0
shreyanshbhardwaj725 <shreyanshbhardwaj725@gmail.com>
2026-04-07 17:45:41 +0530

    Add files via upload

---
+++ 7april26.py
@@ -0,0 +1,10 @@
+def numberOfBits(n):
+    # Count variable set as 0
+    count = 0
+    # Right shift the number till it becomes 0
+    while (n):
+        count += 1
+        n >>= 1
+    return count
+number = int(input("Enter your number : "))
+print("Total bits : ",numberOfBits(number))
